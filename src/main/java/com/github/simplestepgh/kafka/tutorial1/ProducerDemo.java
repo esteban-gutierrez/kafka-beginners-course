9466d13b1f99f68d4abaa3c57ff5261fd62a5ab9
Esteban Gutiérrez Marín <esteban.gutierrez@bestsecret.com>
2020-12-06 19:32:33 +0100

    Old way to initialize properties for the producer (by hard-coding their names)

--- src/main/java/com/github/simplestepgh/kafka/tutorial1/ProducerDemo.java
+++ src/main/java/com/github/simplestepgh/kafka/tutorial1/ProducerDemo.java
@@ -1,7 +1,18 @@
 package com.github.simplestepgh.kafka.tutorial1;
 
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        // 1. Create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // 2. Create the Producer
+        // 3. Send data
     }
 }
